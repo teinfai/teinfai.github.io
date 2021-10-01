9ddd7942f2f071f6e9abbccbc3a7758e7b4f732c
teinfai <teinfai@gmail.com>
2021-10-02 00:59:18 +0800

    cscs

--- geolocation/js/location.js
+++ geolocation/js/location.js
@@ -4,11 +4,12 @@ elLocate.addEventListener("click",function(){
     var a = document.getElementById("demo1");
     var b = document.getElementById("demo2");
 
+    function getlocation() {}
     if(navigator.geolocation){ 
-        navigator.geolocation.getCurrentPosition(function(position){
+        navigator.geolocation.getCurrentPosition(function(position)){
             a.innerHTML = "Latitude: " + position.coords.latitude
             b.innerHTML = "Longitude: " + position.coords.longitude
-        })
+        }
     } else {
         alert("Geolocation is not supported!")
     }
