e518b29dcb0d5f2162374ac867ff5ea238c1afe3
teinfai <teinfai@gmail.com>
2021-10-02 01:01:41 +0800

    scscasc

--- geolocation/js/location.js
+++ geolocation/js/location.js
@@ -6,10 +6,10 @@ elLocate.addEventListener("click",function(){
 
     function getlocation() {}
     if(navigator.geolocation){ 
-        navigator.geolocation.getCurrentPosition(function(position)){
+        navigator.geolocation.getCurrentPosition(function(position){
             a.innerHTML = "Latitude: " + position.coords.latitude
             b.innerHTML = "Longitude: " + position.coords.longitude
-        }
+        })
     } else {
         alert("Geolocation is not supported!")
     }
