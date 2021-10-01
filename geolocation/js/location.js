436532a7c79a5aadf04ac38d8be325a2b89f8550
teinfai <teinfai@gmail.com>
2021-10-02 00:50:46 +0800

    sdada

--- geolocation/js/location.js
+++ geolocation/js/location.js
@@ -1,11 +1,23 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 let elLocate = document.getElementById("btnLocate")
 elLocate.addEventListener("click",function(){
     
 
-    if(navigator.geolocation){
+    if(navigator.geolocation){ 
         navigator.geolocation.getCurrentPosition(function(position){
-            let elLat = document.getElementById("lat")
-            let elLong = document.getElementById("long")
             elLat.innerHTML = "Latitude: " + position.coords.latitude
             elLong.innerHTML = "Longitude: " + position.coords.longitude
         })
