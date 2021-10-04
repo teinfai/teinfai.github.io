36d96169b16d8dc7737ccbee7bdcfc9aa5f9d161
teinfai <teinfai@gmail.com>
2021-10-04 22:58:01 +0800

    Assignment

--- asign/aisgnjs/js.js
+++ asign/aisgnjs/js.js
@@ -1,24 +1,28 @@
-function getMinMax(val) {
+function getMinMax() {
+  var x = document.getElementById("myarr").value;
+  let myFunc = num => Number(num);
+  var val = Array.from(String(x), myFunc);
 
   // Sort the array form lowest to highest number
- sorted=val.sort(function(a,b){return a - b});
+  sorted=val.sort(function(a,b){return a - b});
  
- // Add up all the number
- let text = 0;
- for (let i = 0; i < val.length; i++) {
-   text += val[i];}
+ // Add up all the number for average calculation
+  let text = 0;
+  for (let i = 0; i < val.length; i++) {
+    text += val[i];}
  
  // declare output on specific variable
- average = (text/val.length)
- min = sorted[0]
- max = sorted[sorted.length - 1]
+  average = (text/val.length)
+  min = sorted[0]
+  max = sorted[sorted.length - 1]
  
- // Print variable
- console.log("min=" + min)
- console.log("max=" +max)
- console.log("average=" +average)
+//  Print variable
+  // console.log("min=" + min)
+  // console.log("max=" +max)
+  // console.log("average=" +average)
   
+    
+  document.getElementById("demo").innerHTML =  min;
+  document.getElementById("demo1").innerHTML = max;
+  document.getElementById("demo2").innerHTML = average;
  }
- 
- var aa = [2,3,4,2,3,4,2,3,4,7,8,9,0,12,2];
- getMinMax(aa)
